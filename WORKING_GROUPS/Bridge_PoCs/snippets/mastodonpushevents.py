a9367db925f282ea39049b9d816e369d6ed67386
Thura Aung <thuraaung.variable@gmail.com>
2022-01-05 22:08:18 +0630

    Delete mastodonpushevents.py

--- WORKING_GROUPS/Bridge_PoCs/snippets/mastodonpushevents.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-status_string = "an offer from whatscookin"
-
-headers = {"Authorization": "Bearer OPLtF4AzaDdFS0FkbRv_IqmAdhVn6rtnIrFaXib4MCg"}
-payload = {"status": status_string}
-response = requests.post("https://mastodon.social/api/v1/statuses", data=payload, headers=headers)
-print(response.status_code)
-print(response.text)
