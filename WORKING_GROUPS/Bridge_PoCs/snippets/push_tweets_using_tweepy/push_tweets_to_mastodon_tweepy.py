c1e7758ffd042b0c69f041f8a7683116a85a4e02
tra371 <thuraaung.variable@gmail.com>
2022-07-07 22:24:38 +0630

    implement twitter-mastodon bridge using tweepy

---
+++ WORKING_GROUPS/Bridge_PoCs/snippets/push_tweets_using_tweepy/push_tweets_to_mastodon_tweepy.py
@@ -0,0 +1,38 @@
+#Reference - https://towardsdatascience.com/how-to-extract-data-from-the-twitter-api-using-python-b6fbd7129a33
+
+import os
+import tweepy
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+bearer_token = os.environ["TWITTER_BEARER_TOKEN"]
+MASTODON_ACCESS_TOKEN = os.environ["MASTODON_ACCESS_TOKEN"]
+MASTODON_WEBHOOK_URL = os.environ["MASTODON_WEBHOOK_URL"]
+
+EXCEPTIONS = (tweepy.HTTPException, tweepy.Unauthorized, tweepy.Forbidden, tweepy.NotFound, tweepy.TwitterServerError)
+
+class MyStreamer(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        try:
+            toot_on_mastodon(tweet)
+        except EXCEPTIONS as e:
+            print(e)
+
+def toot_on_mastodon(tweet):
+    headers = {'Authorization': 'Bearer {}'.format(MASTODON_ACCESS_TOKEN)}
+    payload = {'status': tweet['data']['text']}
+
+    try:
+        mastodon_response = requests.post(
+            MASTODON_WEBHOOK_URL, data=payload, headers=headers)
+        mastodon_response.raise_for_status()
+        print("Tweet is sent to mastodon!")
+    except:
+        print("Could not send to mastodon!")
+
+streamer = MyStreamer(bearer_token)
+streamer.add_rules(tweepy.StreamRule("from:bluesky__events -is:retweet"))
+
+streamer.filter()
